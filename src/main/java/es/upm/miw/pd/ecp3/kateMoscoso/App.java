058608d96853e542914a930f1b9acf5bcdca9a76
kateMoscoso <katiemscs@gmail.com>
2014-10-18 13:20:47 +0200

    push del repositorio local

---
+++ src/main/java/es/upm/miw/pd/ecp3/kateMoscoso/App.java
@@ -0,0 +1,13 @@
+package es.upm.miw.pd.ecp3.kateMoscoso;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
